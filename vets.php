fa43ba0fd60d2ed8b0b718d27a7ddbf84c5908be
Jonathan Smith <10632405@my.uvu.edu>
2018-08-09 18:17:47 +0000

    changed up wording

--- vets.php
+++ vets.php
@@ -52,7 +52,7 @@ $currentPage = __FILE__;
                     <div class="card-block px-2">
                         <h4 class="card-title">Dr. Blah Blah</h4>
 
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                        <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
                     </div>
                 </div>
             </div>
@@ -65,7 +65,7 @@ $currentPage = __FILE__;
                         <div class="card-block px-2">
                             <h4 class="card-title">Dr. Blah Blah</h4>
 
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                            <p class="card-text">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.</p>
                         </div>
                     </div>
                 </div>
@@ -78,7 +78,7 @@ $currentPage = __FILE__;
                             <div class="card-block px-2">
                                 <h4 class="card-title">Dr. Blah Blah</h4>
 
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                <p class="card-text">Massa tempor nec feugiat nisl pretium fusce id velit. Integer malesuada nunc vel risus commodo. Maecenas accumsan lacus vel facilisis volutpat. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Venenatis a condimentum vitae sapien pellentesque. Purus gravida quis blandit turpis cursus in. Euismod quis viverra nibh cras pulvinar mattis nunc. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Nisi porta lorem mollis aliquam. Volutpat est velit egestas dui id ornare arcu. Bibendum arcu vitae elementum curabitur vitae nunc sed velit. Malesuada fames ac turpis egestas.</p>
                             </div>
                         </div>
                     </div>
@@ -91,7 +91,7 @@ $currentPage = __FILE__;
                                 <div class="card-block px-2">
                                     <h4 class="card-title">Dr. Blah Blah</h4>
 
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    <p class="card-text">Consectetur adipiscing elit duis tristique. Orci ac auctor augue mauris augue neque gravida. Mauris sit amet massa vitae tortor condimentum lacinia quis. Dolor morbi non arcu risus quis varius quam. Pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Eget mauris pharetra et ultrices neque ornare aenean. Dolor magna eget est lorem ipsum dolor sit amet. Nullam vehicula ipsum a arcu. Eget felis eget nunc lobortis mattis aliquam faucibus purus. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Erat pellentesque adipiscing commodo elit at imperdiet dui. In egestas erat imperdiet sed euismod nisi porta lorem.</p>
                                 </div>
                             </div>
                         </div>
